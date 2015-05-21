acd8e8084ecda5cae27686ad94710051ffd004f3
Dustin Hershman <dustinh17@gmail.com>
2015-05-21 09:54:25 -0500

    Added More functions

--- Rust/Snippets.js
+++ Rust/Snippets.js
@@ -1,4 +1,3 @@
-
 var Snippets = {
 
     /**
@@ -79,6 +78,62 @@ var Snippets = {
         } else {
             return false;
         }
+    },
+
+    /**
+     * Locate a player using their steamID
+     * @method   findPlayerByID
+     * @memberOf Snippets
+     * @param    {String}          playerid [String of the desired players steam id]
+     * @return   {Object}                   [Returns the Rust Base Player Object]
+     */
+    findPlayerByID: function(playerid) {
+        var global = importNamespace("");
+        var targetPlayer = global.BasePlayer.Find(playerid);
+        if (targetPlayer) {
+            return targetPlayer;
+        } else {
+            return false;
+        }
+    },
+
+    /**
+     * Resets data accordingly when called usually called from a command handling switch
+     * @method   resetData
+     * @memberOf Snippets
+     * @param    {Object}          player [The Rust Base Player Object]
+     * @param    {String}          cmd    [String that contains the used command]
+     * @param    {Array}          args   [Array of arguments sent with the command]
+     * @example this.resetData(player, cmd, args)
+     */
+    resetData: function(player, cmd, args) {
+        delete DataObject;
+        data.SaveData("DataFile"); // NOTE: "DataFile" is a filler name, you can name this whatever you want your data file to be.
+        data.DetData("DataFile");
+        player.CharMessage("Successfully Reset Data File");
+    },
+
+    /**
+     * Grabs the Plugin JSON Data file from the data folder then builds default data or loads existing data
+     * @method   getData
+     * @memberOf Snippets
+     * @example this.getData();
+     */
+    getData: function() {
+        DataFile = data.GetData('MyPluginsDataFileName');
+        DataFile = MyPluginData || {}; //looks for MyPluginData object or builds a new one if not found
+        MyPluginData.PlayerData = MyPluginData.PlayerData || {}; //Looks for PlayerData inside MyPluginData or builds a new object if not found.
+    },
+
+    /**
+     * Saves the JSON data file to the oxide data folder when called
+     * @method   saveData
+     * @memberOf Snippets
+     * @example this.saveData();
+     *
+     */
+    saveData: function() {
+        data.SaveData('MyPluginsDataFileName');
     },
 
     /**
@@ -169,7 +224,7 @@ var Snippets = {
     /**
      * Handles KDR changes as a player progresses, takes basic kills and deaths of player to build
      * @method   updateKDR
-     * @memberOf Snippets
+     * @memberOf RanksAndTitles
      * @param    {Array}          ids [Array of ids to be sent to the function]
      * @example this.updateKDR([id1, id2]);
      */
@@ -223,5 +278,140 @@ var Snippets = {
             player.ChatMessage("This group does not exist.");
         }
         return false;
+    },
+
+    /**
+     * Checks the FriendsAPI plugin and Clans Plugin for friends Data
+     * @method   checkForFriends
+     * @memberOf BountyBoard
+     * @param    {String}          steamID   [First players steamID]
+     * @param    {String}          steamID2  [Second Players steamID]
+     * @return   {Boolean}                     [Returns Boolean if the players are friends or clan mates]
+     */
+    checkForFriends: function(steamID, steamID2) {
+        if (this.Config.Settings.antiFriend) {
+            if (friendsAPI && friendsAPI.Call("HasFriend", steamID, steamID2)) {
+                return true;
+            } else if (clansOn && !clansOn.Call("HasFriend", steamID, steamID2)) {
+                attackerClan = clansOn.Call("FindClanByUser", steamID);
+                victimClan = clansOn.Call("FindClanByUser", steamID2);
+                if (attackerClan === victimClan) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        }
+        return false;
+    },
+
+    /**
+     * Gives an item automatically attached to a victim when called
+     * @method   giveItem
+     * @memberOf BountyBoard
+     * @param    {Object}          player   [Rust Base Player Object]
+     * @param    {String}          victimID [Steam Id of the killed user]
+     * @param    {String}          itemName [The item name of the rewarded item]
+     * @param    {Integer}          amount   [Amount of said item to be rewarded]
+     * @example this.giveItem(player, "id", "stones", 1000);
+     */
+    giveItem: function(player, victimID, itemName, amount) {
+        itemName = itemName.toLowerCase();
+        if (itemName === this.Config.Settings.currency) {
+            var econData = econAPI.Call("GetUserDataFromPlayer", player);
+            econData.Call("Deposit", amount);
+        } else if (itemName !== this.Config.Settings.currency) {
+            var definition = global.ItemManager.FindItemDefinition(itemName);
+            if (definition === null) return print("Unable to Find an Item.");
+            print("Giving Item: " + definition.itemid);
+            player.inventory.GiveItem(global.ItemManager.CreateByItemID(Number(definition.itemid), Number(amount), false), player.inventory.containerMain);
+        }
+        this.saveData();
+    },
+
+    /**
+     * Checks a player if he has an item of the same type already in plce on him
+     * @method   checkForDupes
+     * @memberOf BountyBoard
+     * @param    {String}          targetID [Steam ID of the targeted player]
+     * @param    {String}          itemName [Set Item Name]
+     * @param    {Integer}          amt      [Number designated to be the amount of the item]
+     * @return   {Boolean}                   [Returns true or false after removing duplicate items]
+     * @example this.checkForDupes("ID", "wood", 500);
+     */
+    checkForDupes: function(targetID, itemName, amt) {
+        var boardData = BountyData.Board[targetID];
+        var playerData = BountyData.PlayerData[targetID];
+        var i = 0;
+        if (boardData === undefined) return false;
+
+        for (i; i < boardData.Amount.length; i++) {
+            var itemTypeName = boardData.Amount[i].split(" ").pop();
+            if (itemName === itemTypeName) {
+                var storedAmt = boardData.Amount[i].split(" ").shift();
+                var newAmt = Number(storedAmt) + Number(amt);
+                boardData.Amount[i] = newAmt + " " + itemName;
+                boardData.ItemType[i] = itemName;
+                playerData.Bounty[i] = newAmt + " " + itemName;
+                playerData.BountyType[i] = itemName;
+                return true;
+            }
+        }
+        return false;
+    },
+
+    //--------------------------------------------------
+    // There are three types of ways to update a Config
+    // 1. The Standard Way this.LoadDefaultConfig(); Call
+    // 2. The Only check if theres a version change call
+    // 3. The Smart Updater Config Way
+    //--------------------------------------------------
+
+    /**
+     * This is Method 2, check for a version and then update if its not up to date.
+     * Make sure you change the version number in your function!
+     * @method   updateConfig
+     * @memberOf Snippets
+     * @return   {Boolean}          [Default Boolean return to end the function]
+     */
+    updateConfig: function() {
+        if (this.Config.Version !== "1.0") {
+            print("[MyPlugin] Updating Config, to latest version.");
+            this.LoadDefaultConfig();
+            this.SaveConfig();
+        }
+        return false;
+    },
+
+    /**
+     * This is Method 3, it takes a bit more work but it's accurate, and will not erase
+     * a users existing settings or translations. (Makes server owners lives easier) plus
+     * You handle the update and what goes inside the config and how.
+     * @method   smartUpdateConfig
+     * @memberOf Snippets
+     */
+    smartUpdateConfig: function() {
+        var counter = 0;
+        if (this.Config.Version !== "1.0.0") {
+            print("Updating Config to v1.0.0");
+            this.Config.Version = "1.0.0";
+            print("Updating Settings...");
+            this.Config.newSetting = "New String Setting!";
+            counter++;
+            print("Adding New Setting");
+            this.Config.Test.newTest = "Adding New String to a existing object";
+            counter++;
+            print("Adding New String to Test...");
+            print("---------------------------------------")
+            print("Config Successfully Updated to v1.6.4");
+
+            print("Number of changes: " + counter);
+        } else {
+            print("Config already at latest version: v" + this.Config.Version);
+            return false;
+        }
+        this.SaveConfig();
     }
 }
