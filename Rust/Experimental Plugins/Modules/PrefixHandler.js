fea3e028d8fa89b105d9233e09ef7fe34d5ad5fd
Dustin Hershman <dustinh17@gmail.com>
2015-07-07 09:06:56 -0400

    Update to PrefixHandler

--- Rust/Experimental Plugins/Modules/PrefixHandler.js
+++ Rust/Experimental Plugins/Modules/PrefixHandler.js
@@ -1,72 +1,78 @@
-function PrefixHandler(title, author, version, args) {
-  this.Title = title;
-  this.Author = author;
-  this.Version = version;
-  this.RanksData = args[0];
-  this.RanksConfig = args[1];
-  this.RanksConfig.phMsgs = this.RanksConfig.phMsgs || {
-    "noPrefix": "Prefix Not Found",
-    "nonActive": "This Prefix is Currently not active",
-    "success": "Your prefix was changed successfully",
-    "noPerms": "You do not have Permission to use this prefix",
-    "title": "Current Avaliable Prefixes",
-    "info": "Use /ph prefixname to set a prefix"
-  };
-  command.AddChatCommand("ph", this.Plugin, "preifxCmd");
+function prefixHandler(args) {
+  if (args.length > 0) {
+    this.init(args);
+  }
+  else {
+    return false;
+  }
 }
 
-PrefixHandler.prototype = {
+prefixHandler.prototype = {
 
-  OnPlayerInit: function(player) {
-    var steamID = rust.UserIDFromPlayer(player);
-    this.prefixData(steamID);
+  init: function(args) {
+    this.RanksData = args[0];
+    this.RanksConfig = args[1];
+    this.RanksConfig.phMsgs = this.RanksConfig.phMsgs || {
+      "noPrefix": "Prefix Not Found",
+      "nonActive": "This Prefix is Currently not active",
+      "success": "Your prefix was changed successfully",
+      "noPerms": "You do not have Permission to use this prefix",
+      "title": "Current Avaliable Prefixes",
+      "info": "Use /ph prefixname to set a prefix"
+    };
   },
 
-  hasPermission: function(player, perm) {
+  prefixUpdate: function(player, cmd, args) {
+    print("Ran Prefix Command");
+    var uData = data.GetData('oxide.users');
+    try {
     var steamID = rust.UserIDFromPlayer(player);
-    if (player.net.connection.authLevel === 2) {
-      return true;
+    var RanksPrefixes = this.RanksConfig.Main.Prefixes;
+    if (args.length === 1) {
+      this.displayPrefixes(player, uData[steamID].Groups);
+      return false;
     }
 
-    if (permission.UserHasPermission(steamID, perm)) {
-      return true;
-    }
-    rust.SendChatMessage(player, prefix, this.RanksConfig.phMsgs.noPerms, "0");
-    return false;
-  },
-
-  getRanksData: function() {
-    var len = this.RanksConfig.prefixTitles.length,
-      temp = [];
-    for (var i = 0; i < len; i++) {
-      temp.push(this.RanksConfig.prefixTitles[i].title);
-    }
-    return temp;
-  },
-
-  prefixCmd: function(player, cmd, args) {
-    var steamID = rust.UserIDFromPlayer(player);
-    var RanksPrefixes = this.RanksConfig.prefixTitles;
+    if (args.length === 2) {
     for (var i = 0, len = RanksPrefixes.length; i < len; i++) {
-      if (args.length > 0) {
-        if (args[0].toLowerCase() === RanksPrefixes[i].toLowerCase() && this.hasPermission(player, args[0])) {
-          this.RanksData.PlayerData[steamID].Prefix = RanksPrefixes[i];
-          player.SendChatMessage(this.RanksConfig.phMsgs.success);
+      for (var q = 0, uLen = uData[steamID].Groups.length; q < uLen; q++) {
+        if (args[1].toLowerCase() === RanksPrefixes[i].permission.toLowerCase() && RanksPrefixes[i].permission === uData[steamID].Groups[q]) {
+          this.RanksData.PlayerData[steamID].Prefix = uData[steamID].Groups[q].replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+          player.ChatMessage(this.RanksConfig.phMsgs.success);
           data.SaveData('RanksandTitles');
           return false;
         }
-      } else {
-        player.SendChatMessage(this.msgs.title);
-        player.SendChatMessage("--------------------------");
-        if (this.hasPermission(player, RanksPrefixes[i])) {
-          player.SendChatMessage("<color=orange>" + RanksPrefixes[i] + "</color>");
-        }
-        player.SendChatMessage("--------------------------");
-        player.SendChatMessage(this.msgs.info);
+      else if (args.length > 2) {
+        player.ChatMessage(this.RanksConfig.phMsgs.info);
         return false;
       }
     }
-    player.SendChatMessage(this.RanksConfig.phMsgs.noPrefix);
+  }
+}
+    player.ChatMessage(this.RanksConfig.phMsgs.noPerms);
+  } catch(e) {
+    print(e.message.toString());
+  }
     return false;
+  },
+
+  displayPrefixes: function(player, gArr) {
+    player.ChatMessage(this.RanksConfig.phMsgs.title);
+    player.ChatMessage("--------------------------");
+    for (var i = 0, len = gArr.length; i < len; i++) {
+      player.ChatMessage("<color=orange>" + gArr[i] + "</color>");
+    }
+    player.ChatMessage("--------------------------");
+    player.ChatMessage("<color=lime>" + this.RanksConfig.phMsgs.info+ "</color>");
+  }
+};
+
+var PrefixHandler = {
+  Title: "Prefix Handler",
+  Author: "KillParadise",
+  Version: V(1,0,0),
+  Init: function() {
+    print("PrefixHandler Installed. Please Reload RanksAndTitles");
+
   }
 };
