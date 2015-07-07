576e13260291bb0693fe9f39e0a47b0973601cc8
Dustin Hershman <dustinh17@gmail.com>
2015-07-07 11:51:46 -0400

    Fix and Update for ParaAPI moving it to a new repository

--- Rust/Experimental Plugins/Modules/Punishment.js
+++ Rust/Experimental Plugins/Modules/Punishment.js
@@ -2,9 +2,9 @@ function Punishment(title, author, version, args) {
   this.Title = title;
   this.Author = author;
   this.Version = version;
-  this.RanksData = args[0];
-  this.RanksConfig = args[1];
-  this.RanksConfig.punishments = this.RanksConfig.punishments || {
+  this.moduleData = args[0];
+  this.moduleConfig = args[1];
+  this.moduleConfig.punishments = this.moduleConfig.punishments || {
     message: "You killed a fellow <color={color}>{tree}</color> You've lost an additonal <color=orange>{karma}</color>",
     rates: [{
       "rank": 0,
@@ -36,7 +36,7 @@ function Punishment(title, author, version, args) {
 
 Punishment.prototype = {
   punishHandler: function(killer, victimID, killerID) {
-    var punish = this.RanksConfig.punishments.rates;
+    var punish = this.moduleConfig.punishments.rates;
     var karma = 0;
     var replace = {
       color: "lime",
@@ -44,35 +44,39 @@ Punishment.prototype = {
       karma: modifier
     };
     for (var i = 0; i < punish.length; i++) {
-      if (this.RanksData.PlayerData[killerID].Rank === punish[i].rank) {
-        karma = this.RanksConfig.main[i].karmaModifier * punish[i].multiplier;
-        modifier = karma - this.RanksConfig.main[i].karmaModifier;
+      if (this.moduleData.PlayerData[killerID].Rank === punish[i].rank) {
+        karma = this.moduleConfig.main[i].karmaModifier * punish[i].multiplier;
+        modifier = karma - this.moduleConfig.main[i].karmaModifier;
       } else {
         modifier = Math.floor((Math.random() * 40) + 1);
       }
     }
 
-    if (this.RanksData.PlayerData[victimID].Karma >= 0 && this.RanksData.PlayerData[killerID].Karma > 0) {
+    if (this.moduleData.PlayerData[victimID].Karma >= 0 && this.moduleData.PlayerData[killerID].Karma > 0) {
 
-      this.RanksData.PlayerData[killerID].Karma -= modifier;
-      killer.SendChatMessage(this.RanksConfig.punishments.message.replace(/\{color\}|\{tree\}|\{karma\}/gi, function(x){return replace[x];}));
-    } else if (this.RanksData.PlayerData[victimID].Karma < 0 && this.RanksData.PlayerData[killerID].Karma < 0) {
+      this.moduleData.PlayerData[killerID].Karma -= modifier;
+      killer.SendChatMessage(this.moduleConfig.punishments.message.replace(/\{color\}|\{tree\}|\{karma\}/gi, function(x){return replace[x];}));
+    } else if (this.moduleData.PlayerData[victimID].Karma < 0 && this.moduleData.PlayerData[killerID].Karma < 0) {
       replace.color = "red";
       replace.tree = "Bandit";
 
-      this.RanksData.PlayerData[killerID].Karma += modifier;
-      killer.SendChatMessage(this.RanksConfig.punishments.message.replace(/\{color\}|\{tree\}|\{karma\}/gi, function(x){return replace[x];}));
+      this.moduleData.PlayerData[killerID].Karma += modifier;
+      killer.SendChatMessage(this.moduleConfig.punishments.message.replace(/\{color\}|\{tree\}|\{karma\}/gi, function(x){return replace[x];}));
     }
     RanksAndTitles.saveData();
   },
 
   OnEntityDeath: function(entity, hitinfo) {
-    this.RanksData = data.GetData("RanksandTitles");
-    var killer = hitinfo.Initiator,
-      victim = entity;
-    if (killer === null || victim === null || !killer.ToPlayer() || !victim.ToPlayer()) return false;
-    var killerID = rust.UserIDFromPlayer(killer),
-      victimID = rust.UserIDFromPlayer(victim);
-    this.punishHandler(killer, victimID, killerID);
+
+    var dh = API.deathHandler({victim: entity, killer: hitinfo.Initiator});
+    if (dh) this.punishHandler(dh.killer, dh.victimID, dh.killerID);
+    return false;
+    // this.moduleData = data.GetData("RanksandTitles");
+    // var killer = hitinfo.Initiator,
+    //   victim = entity;
+    // if (killer === null || victim === null || !killer.ToPlayer() || !victim.ToPlayer()) return false;
+    // var killerID = rust.UserIDFromPlayer(killer),
+    //   victimID = rust.UserIDFromPlayer(victim);
+    // this.punishHandler(killer, victimID, killerID);
   }
 };
---
+++ Rust/Experimental Plugins/Modules/Ranks.js
@@ -0,0 +1,135 @@
+function RanksSystem(title, author, version, args) {
+    this.Title = title;
+    this.Author = author;
+    this.Version = version;
+    this.moduleData = args[0];
+    this.moduleConfig = args[1];
+    this.moduleConfig.Modules["Ranks"].Version = this.Version;
+    this.moduleConfig.Permissions.Ranks = this.moduleConfig.Permissions.Ranks || {
+
+    };
+    this.moduleConfig.Ranks = this.moduleConfig.Ranks || {
+        Main: {
+            Survivor: {
+                color: "#FFFFFF",
+                rank: 0,
+                killsNeeded: 0
+            },
+            Youngling: {
+                color: "red",
+                rank: 1,
+                killsNeeded: 5
+            }
+        },
+        Messages: {
+            test: "Test Message",
+            slain: "<color=lime>{slayer}</color> the <color={slayerColor}>{title}</color> has slain <color=lime>{slain}</color> the <color={slainColor}>{stitle}</color>!"
+        },
+        Settings: {
+            AntiSleeperOn: false,
+            deathMsgs: true
+        },
+        Permissions: {
+
+        }
+    };
+    command.AddChatCommand("r", this.Plugin, "rankCmd");
+}
+
+RanksSystem.prototype = {
+
+    OnPlayerInit: function(player) {
+        var steamID = rust.UserIDFromPlayer(player);
+        this.buildData(steamID);
+    },
+
+    buildData: function(steamID) {
+        this.moduleData.PlayerData[steamID] = this.moduleData.PlayerData[steamID] || {};
+        this.moduleData.PlayerData[steamID].Name = this.moduleData.PlayerData[steamID].Name || "";
+        this.moduleData.PlayerData[steamID].Title = this.moduleData.PlayerData[steamID].Title || "";
+        this.moduleData.PlayerData[steamID].Rank = this.moduleData.PlayerData[steamID].Rank || 0;
+        this.moduleData.PlayerData[steamID].Kills = this.moduleData.PlayerData[steamID].Kills || 0;
+        this.moduleData.PlayerData[steamID].Deaths = this.moduleData.PlayerData[steamID].Deaths || 0;
+        this.moduleData.PlayerData[steamID].KDR = this.moduleData.PlayerData[steamID].KDR || 0;
+        this.setRank(steamID);
+        API.saveData();
+    },
+
+    OnEntityDeath: function(entity, hitinfo) {
+        var dh = API.deathHandler({
+            victim: entity,
+            killer: hitinfo.Initiator
+        });
+        if (dh) this.handleStats(dh.killer, dh.victim);
+        return false;
+    },
+
+    handleStats: function(killer, victim) {
+        var killerID = rust.UserIDFromPlayer(killer);
+        var victimID = rust.UserIDFromPlayer(victim);
+        if (this.moduleConfig.Ranks.Settings.AntiAbuseOn && API.antiAbuse(victim, killer)) {
+            return false;
+        }
+
+        if (this.moduleConfig.Ranks.Settings.antiSleeper && victim.IsSleeping() && !this.moduleData.PlayerData[victimID]) return false;
+
+        if (killer.displayName !== victim.displayName) {
+
+            this.moduleData.PlayerData[killerID].Kills += 1;
+            this.moduleData.PlayerData[victimID].Deaths += 1;
+            if (this.moduleConfig.Ranks.Settings.deathMsgs) this.buildDeathMsg([killer.displayName, victim.displayName], [killerID, victimID]);
+
+        } else if (victim.displayName === killer.displayName) {
+            this.moduleData.PlayerData[victimID].Deaths += 1;
+        }
+
+        var ids = [victimID, killerID];
+        for (var i = 0, len = ids.length; i < len; i++) {
+            k2d = this.moduleData.PlayerData[ids[i]].Kills / this.moduleData.PlayerData[ids[i]].Deaths;
+            k2d = Math.ceil(k2d * 100) / 100;
+            if (k2d !== "Infinity") {
+                this.moduleData.PlayerData[ids[i]].KDR = k2d;
+            } else {
+                this.moduleData.PlayerData[ids[i]].KDR = this.moduleData.PlayerData[ids[i]].Kills;
+            }
+        }
+        API.saveData();
+        this.setRank(killerID);
+    },
+
+    buildDeathMsg: function(users, ids) {
+        var slayerTitle = this.moduleData.PlayerData[ids[0]].Title;
+        var slainTitle = this.moduleData.PlayerData[ids[1]].Title;
+        rust.BroadcastChat("RANKS", API.buildString(msgs.slain, [users[0], this.moduleConfig.Ranks.Main[slayerTitle].color, slayerTitle, users[1], this.moduleConfig.Ranks.Main[slainTitle].color, slainTitle]));
+    },
+
+    setRank: function(steamID) {
+        for (var key in this.moduleConfig.Main) {
+            if (this.moduleData.PlayerData[steamID].Kills === this.moduleConfig.Main[key].killsNeeded) {
+                this.moduleData.PlayerData[steamID].Title = key;
+                this.moduleData.PlayerData[steamID].Rank = this.moduleConfig.Main[key].rank;
+            }
+        }
+        API.saveData();
+    },
+
+    rankCmd: function(player, cmd, args) {
+      
+    },
+
+    OnPlayerChat: function(arg) {
+        var chatObj = {
+            config: this.moduleconfig.Ranks,
+            player: arg.connection.player,
+            steamID: rust.UserIDFromPlayer(player),
+            titleColor: "",
+            useTitle: this.moduleData.PlayerData[steamID].Title || "",
+            msg: arg.GetString(0, "text");
+        };
+        var cHandle = new chatHandler(chatObj);
+        if (API.modules.PrefixHandler) return null;
+        global.ConsoleSystem.Broadcast("chat.add", chatObj.steamID, cHandle);
+        print(chatObj.player.displayName + ": " + chatObj.msg);
+        return false;
+    }
+};
